8e44c9b3b2da29f27759753f4072b824f9de118c
nianod <arnoldkk422@gmail.com>
2025-04-04 12:17:23 +0300

    progress

--- todo.js
+++ todo.js
@@ -1,5 +1,15 @@
 const todoInput = document.getElementById("todo-Input");
 const todoButton = document.getElementById("subit-todo");
 const todoList = document.getElementById("list");
+const error = document.getElementById("error")
 
 
+todoButton.addEventListener("click", () => {
+
+    const activity = todoInput.value;
+
+    if(activity = "") {
+        error.style.display = "block";
+        error.textContent = "Can't be empty"
+    }
+})
